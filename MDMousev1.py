07909c3c7c3a74f8b57732e7b38da95367536aaf
bjRichardLiu <bjrichardliu@gmail.com>
2023-02-16 15:46:05 -0500

    Update MDMousev1.py

--- MDMousev1.py
+++ MDMousev1.py
@@ -3,7 +3,7 @@ import pyautogui as pg
 
 
 def MIDItoMouse(note):
-    match note:
+    match note.note:
         case 36:
             pg.mouseDown()
         case 37:
@@ -16,27 +16,27 @@ def MIDItoMouse(note):
             pg.click()
         # scroll up
         case 41:
-            pg.scroll(10)
+            pg.scroll(note.velocity / 20)
         # scroll down
         case 43:
-            pg.scroll(-10)
+            pg.scroll(note.velocity / -20)
         # move up
         case 44:
-            pg.move(0, -50)
+            pg.move(0, note.velocity / -3)
         # move down
         case 45:
-            pg.move(0, 50)
+            pg.move(0, note.velocity / 3)
         # move left
         case 46:
-            pg.move(-50, 0)
+            pg.move(note.velocity / -3, 0)
         # move right
         case 47:
-            pg.move(50, 0)
+            pg.move(note.velocity / 3, 0)
         
 
 
 with mido.open_input() as inport:
     for msg in inport:
         if (msg.type == 'note_on'):
-            print(msg.note)
-            MIDItoMouse(msg.note)
+            print(msg.velocity)
+            MIDItoMouse(msg)
