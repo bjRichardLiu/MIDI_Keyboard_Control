b9fee25ffeedd971c0de93d8b1ec58fff4226587
bjRichardLiu <bjrichardliu@gmail.com>
2023-02-16 15:03:07 -0500

    Update MDMousev1.py

--- MDMousev1.py
+++ MDMousev1.py
@@ -4,33 +4,33 @@ import pyautogui as pg
 
 def MIDItoMouse(note):
     match note:
-        case 48:
+        case 36:
             pg.mouseDown()
-        case 49:
+        case 37:
             pg.mouseDown(button='right')
-        case 50:
+        case 38:
             pg.mouseUp()
-        case 51:
+        case 39:
             pg.mouseUp(button='right')
-        case 52:
+        case 40:
             pg.click()
         # scroll up
-        case 53:
+        case 41:
             pg.scroll(10)
         # scroll down
-        case 53:
+        case 43:
             pg.scroll(-10)
         # move up
-        case 60:
+        case 44:
             pg.move(0, -50)
         # move down
-        case 62:
+        case 45:
             pg.move(0, 50)
         # move left
-        case 64:
+        case 46:
             pg.move(-50, 0)
         # move right
-        case 65:
+        case 47:
             pg.move(50, 0)
         
 
